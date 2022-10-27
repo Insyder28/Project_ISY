5543b0007f4f0883254dcdb8453c378039b44d72
Insyder28 <momentje44@gmail.com>
2022-10-27 09:55:30 +0200

    Rewrite functionality for TicTacToe

---
+++ src/TicTacToe/Seed.java
@@ -0,0 +1,15 @@
+package TicTacToe;
+public enum Seed {   // to save as "Seed.java"
+    CROSS("X"), NOUGHT("O"), NO_SEED(" ");
+
+    // Private variable
+    private String icon;
+    // Constructor (must be private)
+    Seed(String icon) {
+        this.icon = icon;
+    }
+    // Public Getter
+    public String getIcon() {
+        return icon;
+    }
+}
