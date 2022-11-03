4816984bd551446d9a09750a7f7593de54b5980c
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2022-11-03 20:39:08 +0100

    Added RuntimeExceptions

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,41 @@
 import networking.GameSocket;
+import networking.ServerException;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            GameSocket gameSocket = new GameSocket("localhost", 7789);
+        try (GameSocket gameSocket = new GameSocket("localhost", 7789)) {
 
             initialise(gameSocket);
-            gameSocket.login("Desktop");
+            gameSocket.login("Erwin");
 
             String[] games = gameSocket.getGameList();
-            gameSocket.subscribe(games[1]);
+            for(var game : games) System.out.println(game);
         }
-        catch (Exception e) {
+        catch (ServerException | IOException e) {
             throw new RuntimeException(e);
         }
+
     }
 
+
+
+
     public static void initialise(GameSocket gameSocket) {
-        gameSocket.onChallengeCancelledEvent.addListener((arguments) -> {
-            System.out.println("OnChallengeCancelled: " + arguments);
-        });
+        gameSocket.onChallengeCancelledEvent.addListener((arguments) -> System.out.println("OnChallengeCancelled: " + arguments));
 
-        gameSocket.onChallengeEvent.addListener((arguments) -> {
-            System.out.println("OnChallenge: " + arguments);
-        });
+        gameSocket.onChallengeEvent.addListener((arguments) -> System.out.println("OnChallenge: " + arguments));
 
-        gameSocket.onMatchEvent.addListener((arguments) -> {
-            System.out.println("OnMatch: " + arguments);
-        });
+        gameSocket.onMatchEvent.addListener((arguments) -> System.out.println("OnMatch: " + arguments));
 
-        gameSocket.onDrawEvent.addListener((arguments) -> {
-            System.out.println("OnDraw: " + arguments);
-        });
+        gameSocket.onDrawEvent.addListener((arguments) -> System.out.println("OnDraw: " + arguments));
 
-        gameSocket.onLossEvent.addListener((arguments) -> {
-            System.out.println("OnLoss: " + arguments);
-        });
+        gameSocket.onLossEvent.addListener((arguments) -> System.out.println("OnLoss: " + arguments));
 
-        gameSocket.onMoveEvent.addListener((arguments) -> {
-            System.out.println("OnMove: " + arguments);
-        });
+        gameSocket.onMoveEvent.addListener((arguments) -> System.out.println("OnMove: " + arguments));
 
-        gameSocket.onWinEvent.addListener((arguments) -> {
-            System.out.println("OnWin: " + arguments);
-        });
+        gameSocket.onWinEvent.addListener((arguments) -> System.out.println("OnWin: " + arguments));
 
         gameSocket.onYourTurnEvent.addListener((arguments) -> {
             System.out.println("OnYourTurn: " + arguments);
