495e8a7fcd98987a5669ca0ccac63804611f35d7
Erwin-Veenhoven <erwinveenhoven2907@gmail.com>
2022-11-03 20:41:49 +0100

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,6 @@ import networking.GameSocket;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            GameSocket gameSocket = new GameSocket("localhost", 7789);
-
-            initialise(gameSocket);
-            gameSocket.login("Desktop");
-
-            String[] games = gameSocket.getGameList();
-            gameSocket.subscribe(games[1]);
-        }
-        catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static void initialise(GameSocket gameSocket) {
-        gameSocket.onChallengeCancelledEvent.addListener((arguments) -> {
-            System.out.println("OnChallengeCancelled: " + arguments);
-        });
-
-        gameSocket.onChallengeEvent.addListener((arguments) -> {
-            System.out.println("OnChallenge: " + arguments);
-        });
-
-        gameSocket.onMatchEvent.addListener((arguments) -> {
-            System.out.println("OnMatch: " + arguments);
-        });
-
-        gameSocket.onDrawEvent.addListener((arguments) -> {
-            System.out.println("OnDraw: " + arguments);
-        });
-
-        gameSocket.onLossEvent.addListener((arguments) -> {
-            System.out.println("OnLoss: " + arguments);
-        });
-
-        gameSocket.onMoveEvent.addListener((arguments) -> {
-            System.out.println("OnMove: " + arguments);
-        });
-
-        gameSocket.onWinEvent.addListener((arguments) -> {
-            System.out.println("OnWin: " + arguments);
-        });
-
-        gameSocket.onYourTurnEvent.addListener((arguments) -> {
-            System.out.println("OnYourTurn: " + arguments);
-            try { gameSocket.move(4); }
-            catch (Exception ignored) { }
-        });
+        // ...
     }
 }
